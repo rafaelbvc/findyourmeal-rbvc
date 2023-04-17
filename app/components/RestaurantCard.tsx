a5034271115ac1312093ee54b2b5d9ce04df5264
rafaelbvc <rafaelbvc@hotmail.com>
2023-04-17 00:31:39 -0300

    v. 0.2.1 - Fetch data services creation (stable)

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,25 +1,27 @@
 import Link from "next/link";
+import { RestaurantCardType } from "../page";
+import Price from "./price";
 
-export default function RestaurantCard() {
+interface Props {
+  restaurant: RestaurantCardType;
+}
+
+export default function RestaurantCard({ restaurant }: Props) {
   return (
     <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-      <Link href="/restaurant/mama-shelter-downstairs">
-        <img
-          src="https://resizer.otstatic.com/v2/photos/wide-huge/3/43566622.jpg"
-          alt="imagem"
-          className="w-full h-36"
-        />
+      <Link href={`/restaurant/${restaurant.slug}`}>
+        <img src={restaurant.main_image} alt="imagem" className="w-full h-36" />
         <div className="p-1">
-          <h3 className="font-bold text-2xl mb-2">Mama Shelter Downstairs</h3>
+          <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
             <p className="ml-2">77 reviews</p>
           </div>
         </div>
         <div className="flex text-reg font-light capitalize">
-          <p className=" mr-3">Contemporary American</p>
-          <p className="mr-3">$$$$$$</p>
-          <p>Los Angeles</p>
+          <p className=" mr-3">{restaurant.cuisine.name}</p>
+          <Price price={restaurant.price}/>
+          <p>{restaurant.location.name}</p>
         </div>
         <p className="text-sm mt-1 font-bold">Booked 29 times today</p>
       </Link>
