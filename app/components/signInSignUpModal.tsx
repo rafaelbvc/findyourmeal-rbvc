9b039800f0afccef31c63d9ff84d3862b3f54e6f
rafaelbvc <rafaelbvc@hotmail.com>
2023-06-02 06:08:18 -0300

    Teste Repository

--- app/components/signInSignUpModal.tsx
+++ app/components/signInSignUpModal.tsx
@@ -4,7 +4,7 @@ function SignInSignUpModal(param: boolean) {
   return (
     <div className="fixed sm:min-w-[20rem] sm:max-w-[25rem] sm:h-auto sm:p-3">
       <div>
-        <p>Title</p>
+        <p>Title - Title</p>
         <input />
       </div>
     </div>
